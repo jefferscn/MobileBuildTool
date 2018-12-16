4e6a883ab225c10ea6e40cc89e7d881b55dd9eaf
zhufengcn <zhufeng0929@163.com>
2018-12-16 16:51:01 +0800

    ios打包

--- buildservice/cordovapack/plistGen.js
+++ buildservice/cordovapack/plistGen.js
@@ -2,6 +2,7 @@ import upload from './util/upload';
 import config from '../config';
 var fs = require('fs-extra');
 var plist = require('plist');
+var url = require('url');
 var manifestJson = {
     "items": [{
         "assets": [{
@@ -20,10 +21,10 @@ var assets = manifestJson.items[0].assets;
 var metadate = manifestJson.items[0].metadata;
 async function plistGen(o, ipaUrl) {
     // return new Promise(function (resolve, reject) {
-    console.log(ipaUrl)
-    var url = encodeURI(ipaUrl);
-    console.log(url);
-    assets[0].url = url;
+    console.log('IPA URL:' + ipaUrl)
+    var manifestUrl = encodeURI(ipaUrl);
+    console.log('manifestUrl:' + manifestUrl);
+    assets[0].url = url.resolve(config.server.baseUrl, manifestUrl);
     metadate['bundle-identifier'] = o.appPackageName;
     metadate['bundle-version'] = o.appVersion;
     metadate['title'] = o.appName;
@@ -31,7 +32,7 @@ async function plistGen(o, ipaUrl) {
     var data = plist.build(manifestJson);
     var fileName = 'manifest.plist';
     await fs.writeFile(fileName, data);
-    const resp= await upload(config.server.upload, fileName);
+    const resp = await upload(config.server.upload, fileName);
     return resp.url;
 };
 module.exports = plistGen;
